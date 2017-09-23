600097142a94cd33b9cdd0515e270c7ad9a63d5b
hyperchi <hyperbolechi@gmail.com>
2017-09-23 17:30:54 -0400

    make account private

--- settings/accounts.py
+++ settings/accounts.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
+import json
+import codecs
+import os
+path = os.path.abspath(".")
 
 # please set this to your own, this is fake accounts
-accounts = [
-    {
-        "id": "",
-        "password": "",
-    },
-    {
-        "id": "",
-        "password": "",
-    }
-]
+
+# user need to specify user.json file to store user ids the format would be
+# [
+#   {
+#         "id": "a",
+#         "password": "b"
+#   },
+#   {
+#         "id": "c",
+#         "password": "d"
+#   }
+# ]
+
+with codecs.open(path + "/settings/user.json", "r", "utf8") as f:
+    accounts = json.loads(f.read())
+
+import pdb
+pdb.set_trace()
+print(1)
